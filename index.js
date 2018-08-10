e24a6139b173461cf77270eadc8c0e245f9b57a3
Pooja  Tetali <pooja.tetali@gmail.com>
2018-08-10 14:42:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+var employeeNames = ['Ada', 'Brendan', 'Ali']; 
+function printBadges(employeeNames){
+  for (i = 0; i<=employeeNames.length; i++){
+    console.log(employeeNames[i]); 
+  }
+}
+
+printBadges(employeeNames); 
